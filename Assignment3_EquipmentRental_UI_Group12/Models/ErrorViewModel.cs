acf3d9ef1917e54411a5bc0e60647bf9ce30ed4f
Juhwan Seo <jseo@conestogac.on.ca>
2025-11-18 01:18:42 -0500

    Create ASP.NET Core MVC UI project (Assignment3_EquipmentRental_UI_Group12)

---
+++ Assignment3_EquipmentRental_UI_Group12/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Assignment3_EquipmentRental_UI_Group12.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
