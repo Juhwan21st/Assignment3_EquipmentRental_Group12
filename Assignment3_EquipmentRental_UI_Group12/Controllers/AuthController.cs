1cdad1793e0b2d91a2ce002b5aa3a8b893839d52
Juhwan21st <jseo9123@conestogac.on.ca>
2025-11-18 06:35:36 -0500

    Create  AuthController

---
+++ Assignment3_EquipmentRental_UI_Group12/Controllers/AuthController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment3_EquipmentRental_UI_Group12.Controllers
+{
+	public class AuthController : Controller
+	{
+		[HttpGet("auth/login")]
+		public IActionResult Login(string returnUrl = "/")
+		{
+			return Challenge(new Microsoft.AspNetCore.Authentication.AuthenticationProperties
+			{
+				RedirectUri = returnUrl
+			}, "Google");
+		}
+
+		[HttpPost("auth/logout")]
+		public IActionResult Logout()
+		{
+			return SignOut(new Microsoft.AspNetCore.Authentication.AuthenticationProperties
+			{
+				// move to home page after logout
+				RedirectUri = Url.Action("Index", "Home")
+			},
+			Microsoft.AspNetCore.Authentication.Cookies.CookieAuthenticationDefaults.AuthenticationScheme);
+		}
+
+		[HttpGet("auth/denied")]
+		public IActionResult AccessDenied()
+		{
+			return Content("Access Denied");
+		}
+	}
+}
